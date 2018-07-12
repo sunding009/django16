f18e7b3a6ea766e03f1d121bfa79052b7662cc32
sunding <mail.qq.sunding@qq.com>
2018-07-12 17:10:55 +0800

    2.2.4 读取数据库中的内容 01

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import Post
+
+
+def home_page(request):
+    posts = Post.objects.all()
+    post_list = list()
+    for count, post in enumerate(posts):
+        post_list.append("No.{0} {1}<br>".format(count, post))
+
+    return HttpResponse(post_list)
